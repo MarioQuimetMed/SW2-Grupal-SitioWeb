f3513eaca9294887a438cd6e2c60492eefdb4955
MarioQuimetMed <mariohappypro@gmail.com>
2025-07-16 17:58:27 -0400

    ajustes

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(distPath, "index.html"));
 });
 
-// Puerto por defecto para DigitalOcean App Platform
+// Digital Ocean espera que la app escuche en el puerto 8080 (predeterminado) o en el puerto proporcionado por variable de entorno
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor Angular escuchando en http://localhost:${PORT}`);
 });
